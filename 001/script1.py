617635de211f4619c5f2580f9e56ce98d7a6fd51
kornel.zemla <kornel.zemla@ocado.com>
2021-12-01 11:18:45 +0100

    day 01

--- 001/script1.py
+++ 001/script1.py
@@ -1,5 +1,14 @@
-# test: 
-# input: 
+# test: 7
+# input: 1548
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
+input_numbers = list(map(lambda line: int(line), lines))
 
+prv_num = None
+result = 0
+for cur_num in input_numbers:
+    if prv_num and (cur_num - prv_num) > 0: 
+        result += 1
+    prv_num = cur_num
+
+print(result)
--- 001/script2.py
+++ 001/script2.py
@@ -1,5 +1,24 @@
-# test: 
-# input: 
+# test: 5
+# input: 1589
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
+input_numbers = list(map(lambda line: int(line), lines))
 
+num_2, num_3, num_4 = None, None, None
+result = 0
+for num_1 in input_numbers:
+    
+    if num_2 and num_3 and num_4:
+        window_1 = num_2 + num_3 + num_4
+        window_2 = num_1 + num_2 + num_3
+        if window_2 > window_1:
+            print(f"{window_1} {window_2}")
+            result += 1
+
+    num_4 = num_3
+    num_3 = num_2
+    num_2 = num_1
+
+    print(f"{num_1} {num_2} {num_3} {num_4}")
+
+print(result)
