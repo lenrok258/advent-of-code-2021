98844b3015833b7b46fc923f6abca766d5a2b5e8
kornel <nospam@nospam.dev.null>
2021-12-02 07:48:13 +0100

    day 2 star 1

--- 002/script1.py
+++ 002/script1.py
@@ -1,5 +1,20 @@
-# test: 
+# test: 150
 # input: 
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
 
+hor_pos = 0
+depth = 0
+
+for l in lines:
+    command_name, command_value = l.split()
+    if command_name == 'forward':
+        hor_pos += int(command_value)
+    if command_name == 'down':
+        depth += int(command_value)
+    if command_name == 'up':
+        depth -= int(command_value);
+
+print(hor_pos)
+print(depth)
+print(hor_pos*depth)
