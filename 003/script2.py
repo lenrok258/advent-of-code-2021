766fc4472dfa29f7095c4c9be03aa9f528c7e471
kornel.zemla <kornel.zemla@ocado.com>
2021-12-03 09:50:32 +0100

    day 3 part 1

--- 003/script2.py
+++ 003/script2.py
@@ -1,5 +1,35 @@
-# test: 
+# test: 198
 # input: 
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
 
+numbers_count = len(lines) 
+counter_1 = [0] * len(lines[0])
+
+gamma = ''
+epsilon = ''
+
+for line in lines:
+    for i, digit in enumerate(line):
+        if digit == '1':
+            counter_1[i] += 1
+
+for c1 in counter_1:
+    count_1 = int(c1)
+    count_0 = numbers_count - count_1
+    if (count_0 >  count_1):
+        gamma += '0'
+    else:
+        gamma += '1'
+
+for c in gamma:
+    if c == '1':
+        epsilon += '0'
+    else:
+        epsilon += '1'
+
+print(gamma)
+print(epsilon)
+print(int(gamma, 2))
+print(int(epsilon, 2))
+print(int(gamma, 2)*int(epsilon, 2))
