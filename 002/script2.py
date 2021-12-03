98a697b57dcfd098fe571e4ca821d763b1d2492c
kornel.zemla <kornel.zemla@ocado.com>
2021-12-03 09:22:00 +0100

    day 2 part 2

--- 002/script2.py
+++ 002/script2.py
@@ -1,5 +1,25 @@
-# test: 
-# input: 
+# test: 150
+# input: 1864715580
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
 
+hor_pos = 0
+depth = 0
+aim = 0
+
+for l in lines:
+    command_name, command_value = l.split()
+    command_value = int(command_value)
+    if command_name == 'forward':
+        hor_pos += command_value
+        depth += aim * command_value
+    if command_name == 'down':
+        aim += command_value
+    if command_name == 'up':
+        aim -= command_value
+
+    print(f"{hor_pos} {depth} {aim}")
+
+print(hor_pos)
+print(depth)
+print(hor_pos*depth)
