174a6212ffe483299dcaebc243cbfb3083122995
kornel.zemla <kornel.zemla@ocado.com>
2021-12-26 21:20:21 +0100

    day 23

--- 023/script2.py
+++ 023/script2.py
@@ -2,22 +2,26 @@ from copy import copy
 from copy import deepcopy
 from functools import lru_cache
 from collections import defaultdict
+from collections import deque
 
 lines = open('input.txt', 'r').read().splitlines()
 
 input_hallway = ('.', '.', 'X', '.', 'X', '.', 'X', '.', 'X', '.', '.')
 
-rooms_list = [[],[],[],[]]
-for i, l in enumerate(lines[2:6]):
-    rooms_list[0].append((l[3], 0))
-    rooms_list[1].append((l[5], 0))
-    rooms_list[2].append((l[7], 0))
-    rooms_list[3].append((l[9], 0))
+input_rooms = (
+    ('B', 'D', 'D', 'D',),
+    ('A', 'C', 'B', 'C',),
+    ('A', 'B', 'A', 'B',),
+    ('D', 'A', 'C', 'C',),
+)
 
-input_rooms = (tuple(rooms_list[0]), tuple(rooms_list[1]), tuple(rooms_list[2]), tuple(rooms_list[3]) )
-    
-stats = defaultdict(lambda:0)
-
+# test values
+# input_rooms = (
+#     ('B', 'D', 'D', 'A',),
+#     ('C', 'C', 'B', 'D',),
+#     ('B', 'B', 'A', 'C',),
+#     ('D', 'A', 'C', 'A',),
+# )
 
 def print_building(hallway, rooms):
     hallway_str = list(map(lambda h:h[0], hallway))
@@ -27,224 +31,134 @@ def print_building(hallway, rooms):
     print()
 
 
-def cost_multiplier(amph_type):
-    return {'A':1, 'B':10, 'C':100, 'D':1000}[amph_type]
+COST_MULTIPLIER = {'A':1, 'B':10, 'C':100, 'D':1000}
+ROOM_NR_TO_EXIT_DOOR = [2,4,6,8]
+ROOM_NR_TO_MATCHING_AMPH = ['A', 'B', 'C', 'D']
+AMPH_TO_MARCHING_ROOM_NR = {'A':0, 'B':1, 'C':2, 'D':3}
 
 
-def ampth_type_to_room_nr(amph_type):
-    return {'A':0, 'B':1, 'C':2, 'D':3}[amph_type]
+def any_unmatching_amph_bellow(room, amph_i, matching_amph):
+    for i in range(amph_i + 1, 4):
+        if room[i] != matching_amph:
+            return True
+    return False
 
 
-def is_final_state(rooms):
-    target_residents = ['A', 'B', 'C', 'D']
-    for i, room in enumerate(rooms):
-        room_set = set(list(map(lambda a:a[0], room)))
-        if len(room_set) == 1 and list(room_set)[0] is target_residents[i]:
-            continue
-        else:
+def get_room_empty_index_if_possible_to_enter(room, ampt_type):
+    deepest_empty_spot = None
+    for amph_i in range(0, 4):
+        if room[amph_i] == '.':
+            deepest_empty_spot = amph_i
+    if deepest_empty_spot != None:
+        if not any_unmatching_amph_bellow(room, deepest_empty_spot, ampt_type):
+            return deepest_empty_spot
+    return None
+
+
+def hallway_empty_spots_indices(hallway):
+    indices = []
+    for i, s in enumerate(hallway):
+        if s == '.':
+            indices.append(i)
+    return indices
+
+
+def return_cost_if_path_possible(hallway, start_i, stop_i):
+    assert hallway[stop_i] in ['.', 'X']
+
+    step = 1 if start_i <= stop_i else -1
+    range_start = start_i+1 if start_i <= stop_i else start_i-1
+    range_end = stop_i+1 if start_i <= stop_i else stop_i-1
+    cost = 0
+    for i in range(range_start, range_end, step):
+        if hallway[i] not in ['.', 'X']:
+            return None
+        cost += 1
+    return cost
+
+
+def move(hallway, rooms, hallway_i, hallway_v, room_i, amph_i, amph_v):
+    new_hallway = list(copy(hallway))
+    new_hallway[hallway_i] = hallway_v
+    new_rooms = []
+    for i, r in enumerate(rooms):
+        new_room = list(copy(r))
+        if i == room_i:
+            new_room[amph_i] = amph_v
+        new_rooms.append(tuple(new_room))
+    return (tuple(new_hallway), tuple(new_rooms))
+
+
+def moves_from_rooms_to_hallway(hallway, rooms):
+    moves = []
+    for room_i, room in enumerate(rooms):
+        for amph_i, amph in enumerate(room):
+            matching_amph = ROOM_NR_TO_MATCHING_AMPH[room_i]
+            # empty space
+            if amph == '.':
+                continue
+            # 1. can move it if in wrong room
+            # 2. can move if in right room but unmatching ampth bellow
+            if amph != matching_amph or (amph == matching_amph and any_unmatching_amph_bellow(room, amph_i, matching_amph)):
+                exit_door = ROOM_NR_TO_EXIT_DOOR[room_i]
+                for hallway_empty_spot_i in hallway_empty_spots_indices(hallway):
+                    hallway_cost = return_cost_if_path_possible(hallway, exit_door, hallway_empty_spot_i)
+                    if hallway_cost == None:
+                        continue
+                    new_hallway, new_rooms = move(hallway, rooms, hallway_empty_spot_i, amph, room_i, amph_i, '.')
+                    cost = (amph_i + 1 + hallway_cost) * COST_MULTIPLIER[amph]
+                    moves.append((new_hallway, new_rooms, cost))
+                break
+            # sanity check
+            if amph == matching_amph and not any_unmatching_amph_bellow(room, amph_i, matching_amph):
+                continue
+            assert False
+    return moves
+
+
+def moves_from_hallway_to_rooms(hallway, rooms):
+    moves = []
+    for spot_i, spot in enumerate(hallway):
+        if spot not in ['.', 'X']:
+            amph = spot
+            matching_room_nr = AMPH_TO_MARCHING_ROOM_NR[amph]
+            exit_door_nr = ROOM_NR_TO_EXIT_DOOR[matching_room_nr]
+            hallway_cost = return_cost_if_path_possible(hallway, spot_i, exit_door_nr)
+            if hallway_cost == None:
+                continue
+            room = rooms[matching_room_nr]
+            room_empty_i = get_room_empty_index_if_possible_to_enter(room, amph)
+            if room_empty_i == None:
+                continue
+            new_hallway, new_rooms = move(hallway, rooms, spot_i, '.', matching_room_nr, room_empty_i, amph)
+            cost = (hallway_cost + 1 + room_empty_i) * COST_MULTIPLIER[amph]
+            moves.append((new_hallway, new_rooms, cost))
+    return moves
+
+
+@lru_cache(maxsize=None)
+def all_possible_moves(hallway, rooms):
+    moves = []
+    moves_h2r = moves_from_hallway_to_rooms(hallway, rooms)
+    moves_r2h = moves_from_rooms_to_hallway(hallway, rooms)
+    moves.extend(moves_h2r)
+    moves.extend(moves_r2h)
+    return moves
+
+
+def winnig_state(rooms):
+    for room_i, room_v in enumerate(rooms):
+        expected_amph_type = ROOM_NR_TO_MATCHING_AMPH[room_i]
+        amphs_with_matching_type = list(filter(lambda a: a==expected_amph_type, room_v))
+        if len(amphs_with_matching_type) != 4:
             return False
     return True
 
 
-def room_to_hallway_position(room_nr):
-    return [2, 4, 6, 8][room_nr]
-
-
-def hallway_position_to_room_nr(hallway_pos):
-    return [None,None,0,None,1,None,2,None,3,None,None][hallway_pos]
-
-
-def copy_alter(hallway, rooms, room_nr, room_pos, room_new_value, hallway_pos, hallway_new_value):
-    hallway_list = list(hallway)
-    hallway_list[hallway_pos] = hallway_new_value
-    new_hallway = tuple(hallway_list)
-
-    rooms_list = []
-    for room in rooms:
-        room_list = list(room)
-        rooms_list.append(room_list)
-    rooms_list[room_nr][room_pos] = room_new_value
-    new_rooms = (tuple(rooms_list[0]), tuple(rooms_list[1]), tuple(rooms_list[2]), tuple(rooms_list[3]))
-
-    return (new_hallway, new_rooms)
-
-
-def find_free_spot_in_room(room_nr, rooms, amph_type):
-    room = rooms[room_nr]
-    
-    # If it is not your room, do not enter
-    matching_room = ampth_type_to_room_nr(amph_type)
-
-    if matching_room != room_nr:
-        return None
-
-    # If there is a wrong amph in the same room, do not go there or it will be blocked
-    for spot_i in range(4):
-        spot_v = room[spot_i]
-        if spot_v[0] != '.' and spot_v[0] != amph_type:
-            return None
-
-    # Look for the first one with '.' (empty space)
-    for spot_i in reversed(range(4)): #3, 2, 1, 0
-        spot_v = room[spot_i]
-        if spot_v[0] == '.':
-            return spot_i
-   
-    return None
-
-
-def moves_from_hallway_to_room(hallway, rooms):
-
-    states = []
-
-    for hall_i, hall_v in enumerate(hallway):
-        if hall_v in ['X', '.']:
-            continue
-        else:
-            # go left
-            for hallway_move in range(1, hall_i + 1):
-                new_pos_candidate = hall_i - hallway_move
-                if hallway[new_pos_candidate] == 'X':
-                    room_nr = hallway_position_to_room_nr(new_pos_candidate)
-                    free_spot_in_room = find_free_spot_in_room(room_nr, rooms, hall_v[0])
-                    if free_spot_in_room == None:
-                        continue
-                    new_hallway, new_rooms = copy_alter(hallway, rooms, room_nr, free_spot_in_room, hall_v, hall_i, '.')
-                    cost = (hallway_move + 1 + free_spot_in_room) * cost_multiplier(hall_v[0])
-                    states.append((new_hallway, new_rooms, cost))
-                elif hallway[new_pos_candidate] == '.':
-                    continue
-                else:
-                    # someone stands on the way
-                    break
-            
-            # go right
-            for hallway_move in range(1, len(hallway)-hall_i):
-                new_pos_candidate = hall_i + hallway_move
-                if hallway[new_pos_candidate] == 'X':
-                    room_nr = hallway_position_to_room_nr(new_pos_candidate)
-                    free_spot_in_room = find_free_spot_in_room(room_nr, rooms, hall_v[0])
-                    if free_spot_in_room == None:
-                        continue
-                    new_hallway, new_rooms = copy_alter(hallway, rooms, room_nr, free_spot_in_room, hall_v, hall_i, '.')
-                    cost = (hallway_move + 1 + free_spot_in_room) * cost_multiplier(hall_v[0])
-                    states.append((new_hallway, new_rooms, cost))
-                elif hallway[new_pos_candidate] == '.':
-                    continue
-                else:
-                    # someone stands on the way
-                    break
-    
-    return states
-
-
-def moves_from_room_to_hallway(hallway, rooms):
-    states = []
-
-    for room_nr, room in enumerate(rooms):
-        for amph_i, amph in enumerate(room):
-            # empty spot, try another amph in this room
-            if amph[0] == '.':
-                continue
-
-            if amph[1] == 0: # 0 == not visited hallway yet
-                room_halway_pos = room_to_hallway_position(room_nr)
-                # move left
-                for hallway_move in range(0, room_halway_pos):
-                    hallway_position_candidate = room_halway_pos - 1 - hallway_move
-                    
-                    # not allowed to stand here
-                    if hallway[hallway_position_candidate] == 'X':
-                        continue
-
-                    # valid move, create new game state and compute cost
-                    elif hallway[hallway_position_candidate] == '.':
-                        # create move
-                        new_hallway, new_rooms = copy_alter(hallway, rooms, room_nr, amph_i, '.', hallway_position_candidate, (amph[0], 1))
-                        cost = (1 + amph_i + 1 + hallway_move) * cost_multiplier(amph[0])
-                        states.append((new_hallway, new_rooms, cost))
-                    else: # some is already here
-                        break
-
-                # move right
-                for hallway_move in range(0, len(hallway) - room_halway_pos - 1):
-                    hallway_position_candidate = room_halway_pos + 1 + hallway_move
-                    
-                    # not allowed to stand here
-                    if hallway[hallway_position_candidate] == 'X':
-                        continue
-
-                    # valid move, create new game state and compute cost
-                    elif hallway[hallway_position_candidate] == '.':
-                        # create move
-                        new_hallway, new_rooms = copy_alter(hallway, rooms, room_nr, amph_i, '.', hallway_position_candidate, (amph[0], 1))
-                        cost = (1 + amph_i + 1 + hallway_move) * cost_multiplier(amph[0])
-                        states.append((new_hallway, new_rooms, cost))
-                    else: # some is already here
-                        break
-
-            # only first one can move
-            break
-
-    return states
-
-# @lru_cache(maxsize=None)
-def make_all_possible_moves(hallway, rooms):
-    stats["tick"] += 1
-    if stats["tick"] % 100_000 == 0:
-        print(f"STATS: tick: {stats['tick']}, states checked: {stats['states_checked']}, no move: {stats['no_move']}, solusions: {stats['solution_found']}, current_min_solution: {stats['current_min_solution']}" ) 
-        # print_building(hallway, rooms)
-
-    states = []
-
-    # move from hallway to room
-    moves_from_hallway = moves_from_hallway_to_room(hallway, rooms)
-    states.extend(moves_from_hallway)
-
-    # move from room to hallway
-    moves_from_room = moves_from_room_to_hallway(hallway, rooms)
-    states.extend(moves_from_room)
-
-    if not states:
-        stats["no_move"] += 1 
-    
-    stats["states_checked"] += len(states) 
-
-    return states
-
-
-@lru_cache(maxsize=None)
-def tick(hallway, rooms):
-    
-    if is_final_state(rooms):
-        stats["solution_found"] += 1
-        # stats["current_min_solution"] = min(stats["current_min_solution"], cost)
-        # print(f"Found solution!: {cost}")
-        # print_building(hallway, rooms)
-        return [0]
-
-    all_possible_costs = []
-    for new_state in make_all_possible_moves(hallway, rooms):
-        new_hallway, new_rooms, last_move_cost = new_state
-        # new_costs = tick(new_hallway, new_rooms, cost + last_move_cost)
-        new_costs = tick(new_hallway, new_rooms)
-        new_costs_plus_last_move = list(map(lambda c: c+last_move_cost, new_costs))
-        # print(new_costs_plus_last_move)
-        all_possible_costs.extend(new_costs_plus_last_move)
-        # print(f"Path computed {all_possible_costs}")
-
-    return all_possible_costs
-
-
-# print(is_final_state(rooms))
-
-# print(tick(hallway, rooms, 0))
-
-# print(min(tick(hallway, rooms, 0)))
-
 print_building(input_hallway, input_rooms)
 
-# moves = make_all_possible_moves(hallway, rooms)
+
+# moves = all_possible_moves(input_hallway, input_rooms)
 # for move in moves:
 #     hallway, rooms, cost = move
 #     print(f"cost: {cost}")
@@ -252,7 +166,27 @@ print_building(input_hallway, input_rooms)
 # print()
 # print(f"avilable moves: {len(moves)}")
 
-stats["current_min_solution"] = 9999999999999
-result = tick(input_hallway, input_rooms)
-print(f"ALL POSSIBLE COSTS: {result}")
-print(max(result))
+
+moves = all_possible_moves(input_hallway, input_rooms)
+moves_to_check = deque()
+moves_to_check.extend(list(map(lambda m: (0, m), moves)))
+
+results_unique = set()
+min_result = 9999999999999
+for i in range(1_000_000):
+    current_cost, (new_hallway, new_rooms, additional_cost) = moves_to_check.pop()
+    new_cost = current_cost + additional_cost
+    if winnig_state(new_rooms):
+        min_result = min(min_result, new_cost)
+        results_unique.add(new_cost)
+    else:
+        moves_to_add = all_possible_moves(new_hallway, new_rooms)
+        moves_to_check.extend(list(map(lambda m: (new_cost, m), moves_to_add)))
+
+    if (i % 10_000 == 0):
+        print(f"{i} {len(moves_to_check)} {min_result}")
+
+
+print(len(moves_to_check))
+print(min_result)
+print(f"Unique results = {results_unique}")
